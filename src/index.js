f3e870f79327404c24c6603e597dca2180f57dd1
Maja <pedic.maja@gmail.com>
2022-01-13 11:37:07 +0100

    Adds scss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+require('./scss/main.scss')
+
 ReactDOM.render(
   <React.StrictMode>
       <BrowserRouter>
