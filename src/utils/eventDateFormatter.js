9dcdac0dce0abcb29b3c5380bcd72e4476bdb454
Maja <pedic.maja@gmail.com>
2022-01-17 20:59:58 +0100

    Fixes default function export

--- src/utils/eventDateFormatter.js
+++ src/utils/eventDateFormatter.js
@@ -4,6 +4,6 @@ import {parse, parseISO} from 'date-fns'
  * @param {Object} event - Google event object
  * @param {String} timestampType - which event property we need to check for
  */
-export default function (event, timestampType) {
+export default function googleEventDateFormatter (event, timestampType) {
     return event[timestampType].dateTime ? parseISO(event[timestampType].dateTime) : parse(event[timestampType].date, 'yyyy-MM-dd', new Date())
 }
